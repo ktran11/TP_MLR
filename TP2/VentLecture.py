ae3347eaf172a64c531143f628a0ef5e87221da0
Tran Kevin <kevin.tran@etudiant.univ-rennes1.fr>
2022-02-05 10:40:33 +0100

    new TP

---
+++ TP2/VentLecture.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 17:44:34 2020
+
+@author: delyon
+
+"""
+
+import numpy as np
+
+# Adresse des deux fichiers de donnees
+# https://perso.univ-rennes1.fr/bernard.delyon/tp/WindSpeed.csv
+# https://perso.univ-rennes1.fr/bernard.delyon/tp/Hs.csv
+
+etudiant = 14003508
+ # nombre à remplacer par votre numéro d'etudiant
+np.random.seed(etudiant)
+
+# Lecture des donnees
+nomvar=np.loadtxt("data/Vent.csv",delimiter=',',dtype='str',max_rows=1)[1:]
+X=np.loadtxt("data/Vent.csv",delimiter=',',skiprows=1,usecols=1+np.arange(len(nomvar)))
+y=np.loadtxt("data/Vagues.csv",delimiter=',',skiprows=1,usecols=1)
+n=len(y)
+
+# On extrait un sous-ensemble d'individus
+select=np.random.choice(n,size=n//8, replace=False)
+X=X[select,:]
+y=y[select]
+n=n=len(y)
