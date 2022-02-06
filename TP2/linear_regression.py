33b0b01ef042411fbae953ea1c5274264161ee0f
Agathe Blanvillain <agathe.blanvillain.1@etudiant.univ-rennes1.fr>
2022-02-06 10:56:54 +0100

    linear regression

--- TP2/linear_regression.py
+++ TP2/linear_regression.py
@@ -27,22 +27,43 @@ n=len(y)
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-
-#Calcul manuel avec la formule  classique
-X1=np.append(np.ones((len(y),1)),X,1)
-Xt=X1.T
-Ri=np.linalg.inv(Xt.dot(X1))
-bet=Ri.dot((Xt).dot(y))#beta = (XTX)-1.XT.y
-yhat=X1.dot(bet)#yhat = X1.beta
-yhat
-
+mod = LinearRegression()
+mod.fit(X=X,y=y) #fait le calcul
+yhat = mod.predict(X)
+sighat = np.sqrt(sum((yhat-y)**2)/len(y))
 plt.close()
-plt.plot(y,yhat,'r+')
-plt.xlabel('Reponse')
-plt.ylabel("Prediction")
-plt.plot(y,y,color='black')
-plt.title('Droite de regression')
+plt.plot(y,mod.predict(X),'r+')#donne une idée de la variabilité des erreurs
+plt.xlabel('Réponse')
+plt.ylabel("Prédiction")
+plt.plot(y,y, color = 'black')
+plt.title('Droite de regression X 3000 individus')
 plt.show()
+
+
+#### Calcul de l'erreur par validation croisée
+
+
+# 5-Fold
+err=0
+it=100
+ntest=np.around(len(y)/5).astype(int)
+for i in range(it):
+  per = np.random.permutation(X.shape[0])
+  lt, la = per[:ntest], per[ntest:]
+  Xa, Xt = X[la,:], X[lt,:]
+  ya, yt = y[la], y[lt]
+  reg = LinearRegression()
+  reg.fit(Xa,ya)
+  yh=reg.predict(Xt)
+  err+=np.mean((yh-yt)**2)
+err = np.sqrt(err/it)
+print('Erreur par CV 1/5 = ',round(err, 2))
+
+
+
+X = np.multiply(X,X)
+
+
 
 mod = LinearRegression()
 mod.fit(X=X,y=y) #fait le calcul
@@ -53,18 +74,16 @@ plt.plot(y,mod.predict(X),'r+')#donne une idée de la variabilité des erreurs
 plt.xlabel('Réponse')
 plt.ylabel("Prédiction")
 plt.plot(y,y, color = 'black')
+plt.title('Droite de regression X**2 3000 individus')
 plt.show()
-X1=np.append(np.ones((len(y),1)),X,1)
-Xt=X1.T
-Ri=np.linalg.inv(Xt.dot(X1))
-np.sqrt(np.diag(Ri))*sighat
+
 
 #### Calcul de l'erreur par validation croisée
 
 
 # 5-Fold
 err=0
-it=20
+it=100
 ntest=np.around(len(y)/5).astype(int)
 for i in range(it):
   per = np.random.permutation(X.shape[0])
